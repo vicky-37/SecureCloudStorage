b54c6f34baed2227b21340843cb27d4d27aec6bf
vicky-37 <100691440+vicky-37@users.noreply.github.com>
2025-04-28 11:01:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000;
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'eu-north-1 (Stockholm)', // Change if your bucket is in a different region
+  region: 'eu-north-1', // Change if your bucket is in a different region
 });
 
 const s3 = new AWS.S3();
