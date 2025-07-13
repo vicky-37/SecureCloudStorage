757385fe3f8d3957e823467dcdfbbe6fe1904ba5
vicky-37 <100691440+vicky-37@users.noreply.github.com>
2025-07-13 11:15:28 +0530

    Update api.js

--- secure-cloud-frontend/src/api.js
+++ secure-cloud-frontend/src/api.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const API = axios.create({
   baseURL: 'https://hot-dominant-horizon-larry.trycloudflare.com',
 });
+export const registerUser = (credentials) => API.post('/register', credentials);
+export const loginUser = (credentials) => API.post('/login', credentials);
 
 // Upload file
 export const uploadFile = (file) => {
