4fc8cdc0d36e1edcf07129a4c9149e15b381b443
vicky-37 <100691440+vicky-37@users.noreply.github.com>
2025-07-13 11:09:41 +0530

    Update api.js

--- secure-cloud-frontend/src/api.js
+++ secure-cloud-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://13.53.37.186:5000/',
+  baseURL: 'https://hot-dominant-horizon-larry.trycloudflare.com',
 });
 
 // Upload file
